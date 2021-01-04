87a08e74b511a7059ac856673c4feeb626be216c
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-01-04 20:12:43 -0300

    packagejson

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var express = import("express.js")
+const express = require("express")
+const app = express()
+
+
+app.get("/",function(req,res){
+   res.send("tudo certo meu chapa")
+})
+
+
+
+
+
+app.listen(8000, function(){
+    console.log("site funcionando");
+});
