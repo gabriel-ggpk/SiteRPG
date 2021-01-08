6c49fb2c23740a4622b80132dcf0aff9b9820023
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-01-07 21:55:37 -0300

    ta bonito

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/views'));
 
 app.get("/",function(req,res){
    res.render("index.html")
